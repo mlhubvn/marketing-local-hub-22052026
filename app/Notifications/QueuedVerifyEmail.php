c5b971dd2936f3db99547ff0f570e30dae277cfe
codegignow <khoa.mlhub@gmail.com>
2026-06-12 18:19:06 +0700

    fix: add Queueable trait to QueuedVerifyEmail for Laravel 13

--- app/Notifications/QueuedVerifyEmail.php
+++ app/Notifications/QueuedVerifyEmail.php
@@ -3,8 +3,10 @@
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class QueuedVerifyEmail extends VerifyEmail implements ShouldQueue
 {
+    use Queueable;
 }
